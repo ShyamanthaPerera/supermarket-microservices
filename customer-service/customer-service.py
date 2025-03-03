798e3c568a99fc5d3e236371bba0b71d73861daf
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2025-03-03 10:04:48 +0530

    register customer service to api gateway

--- customer-service/customer-service.py
+++ customer-service/customer-service.py
@@ -1,11 +1,19 @@
 from flask import Flask, jsonify
+import py_eureka_client.eureka_client as eureka_client
 
 app = Flask(__name__)
 
 CONTEXT_PATH = "/customer-service"
 SERVICE_PORT = 5000
+EUREKA_SERVER = "http://localhost:8761/eureka/"
 
-# http://127.0.0.1:5000/customer-service/customer
+eureka_client.init(
+    eureka_server = EUREKA_SERVER,
+    app_name = "CUSTOMER-SERVICE",
+    instance_port = SERVICE_PORT
+)
+
+# http://127.0.0.1:5000/customer-service/customers
 @app.route(f'{CONTEXT_PATH}/customers', methods=["GET"])
 def get_customer():
     customer_list=[
